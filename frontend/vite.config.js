1e25eac2eb3dcb06d83a682118eb01e61164f085
Double-Cloth <Double-Cloth@outlook.com>
2026-03-30 21:24:10 +0800

    修复域名无法访问

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -65,7 +65,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'all',
+    allowedHosts: ["your-lugu-lake.duckdns.org"],,
     proxy: {
       '/api': {
         target: process.env.VITE_PROXY_TARGET || 'http://127.0.0.1:8000',
